3d43430fd2b51c670a7cccf7b44b499ad8c759e4
JKD111995 <jake.duque@gmail.com>
2022-10-01 00:55:14 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = generateDomain();
+  });
+};
+
+let generateDomain = () => {
+  let pronoun = ["the", "a", "for", "at", "in"];
+  let adj = ["new", "bright", "dark", "fresh", "shiny", "great"];
+  let noun = ["car", "nature", "food", "light", "window", "wand"];
+  let dom = [".com", ".net", ".org", ".edu"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let domIndex = Math.floor(Math.random() * dom.length);
+
+  return (
+    pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + dom[domIndex]
+  );
 };
